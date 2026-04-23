199a46fa9694e94e4075e1cf89e2bf285bbb2da9
lucasvcantarelli <lucasvcantarellidev@gmail.com>
2026-04-23 12:58:27 -0300

    adicionando o scripts do enviou form

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector("input[name=nome]").value;
+    const email = document.querySelector("input[name=email]").value;
+    const mensagem = document.querySelector("textarea[name=mensagem]").value;
+
+   fetch("https://api.sheetmonkey.io/form/cKsJcX5RFdV6itvWYPp2Py", {
+
+        method: "POST",
+        headers: {
+          "Accept":  "application/json",
+            "Content-Type": "application/json", 
+        },
+        body: JSON.stringify({name: name, email: email, mensagem: mensagem})
+        
+   }).then(() => {
+        alert("Mensagem enviada com sucesso!");
+        document.querySelector("form").reset();
+    }).catch(() => alert("Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente."));
+
+
+}
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
